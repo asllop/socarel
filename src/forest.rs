1cd97b9e6b946d797e815bba73c0aff98eafd531
Andreu Santaren Llop <andreu.santaren@gmail.com>
2021-10-06 10:20:14 +0200

    Move impl_tree_id_traits! macro to forest module.

--- src/forest.rs
+++ src/forest.rs
@@ -3,6 +3,50 @@ use std::collections::hash_map::Iter;
 use crate::node::*;
 use crate::tree::*;
 
+/// Generate a default implementation for [`TreeIdentifier`] dependency traits (PartialEq, Eq, Hash and Display).
+/// 
+/// # Example
+/// 
+/// ```
+/// # use socarel::*;
+/// struct MyTreeId(String);
+/// 
+/// impl TreeIdentifier for MyTreeId {
+///     fn new(tree_id: &str) -> Option<Self> {
+///         Some(Self(String::from(tree_id)))
+///     }
+///     fn get_id(&self) -> &str {
+///         &self.0
+///     }
+/// }
+/// 
+/// impl_tree_id_traits!(MyTreeId);
+/// ```
+#[macro_export]
+macro_rules! impl_tree_id_traits {
+    ( $x:ty ) => {
+        impl std::cmp::PartialEq for $x {
+            fn eq(&self, other: &Self) -> bool {
+                self.get_id() == other.get_id()
+            }
+        }
+        
+        impl std::cmp::Eq for $x {}
+        
+        impl std::hash::Hash for $x {
+            fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+                self.get_id().hash(state);
+            }
+        }
+        
+        impl std::fmt::Display for $x {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", self.get_id())
+            }
+        }
+    };
+}
+
 /// Trait to define structs that model a tree ID
 pub trait TreeIdentifier: std::cmp::Eq + std::hash::Hash + std::fmt::Display {
     //TODO return a Result with wither Self or an error (impl Error trait).
